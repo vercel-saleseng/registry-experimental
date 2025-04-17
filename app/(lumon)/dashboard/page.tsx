c82f7f975152b21cf072d472f8ac9085971ffd49
Derek Wu <derekwu35@gmail.com>
2025-04-17 18:03:19 -0500

    some bug fixes

--- app/(lumon)/dashboard/page.tsx
+++ app/(lumon)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "../../../components/app-sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,15 +8,11 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Page() {
   return (
-    <SidebarProvider>
+    <>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -329,6 +325,6 @@ export default function Page() {
           </div>
         </div>
       </SidebarInset>
-    </SidebarProvider>
+    </>
   );
 }
